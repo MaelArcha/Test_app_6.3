2c8fa59c459e4b1117745b72e1e9aa89e49ff0f8
Hamdi Guelleh <guellehhamdi@gmail.com>
2022-02-08 18:43:47 -0700

    Add User Id in BackEnd

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_001144) do
+ActiveRecord::Schema.define(version: 2022_02_08_213215) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "username"
   end
 
   create_table "users", force: :cascade do |t|
