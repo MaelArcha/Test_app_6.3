199af35b96dda6cddbc1c349df68a5d1ada7c787
Hamdi Guelleh <guellehhamdi@gmail.com>
2022-02-10 20:53:33 -0700

    Add Edit/Update function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :user_set, only: [:edit, :update, :destroy]
+  
   def new
     @user = User.new 
   end
@@ -12,9 +14,31 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+
+  def update
+   if @user.update(user_params)
+    flash[:notice] = "Your Account Information was Successfully Updated"
+    redirect_to articles_path 
+   else
+    render 'edit'
+   end
+  end
+
+  def destroy
+    @user.destroy 
+    redirect_to @user 
+  end
+
 
   private 
   
+  
+  def user_set
+    @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:username, :email, :password)
