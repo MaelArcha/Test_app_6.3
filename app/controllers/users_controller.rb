0fec1a768a17c57c3b517279ec1f908ec81c75e1
Hamdi Guelleh <guellehhamdi@gmail.com>
2022-02-14 20:23:42 -0700

    Pagination & layout cleaning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_action :user_set, only: [:edit, :show, :update, :destroy]
   
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5 ) 
+  end
+  
+  
   def new
     @user = User.new 
   end
@@ -21,14 +27,14 @@ class UsersController < ApplicationController
   def update
    if @user.update(user_params)
     flash[:notice] = "Your Account Information was Successfully Updated"
-    redirect_to articles_path 
+    redirect_to @user 
    else
     render 'edit'
    end
   end
 
   def show
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5) 
   end
 
   def destroy
